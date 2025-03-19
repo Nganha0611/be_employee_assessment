fd36a7d327da1525507a219c8a918ae3b5748c85
Vo Nam Ngan Ha <vonamnganha0611@gmail.com>
2025-03-19 23:15:49 +0700

    Initial commit

---
+++ src/main/java/cnpm/nlu/be_empolyy_assessment/BeEmpolyyAssessmentApplication.java
@@ -0,0 +1,13 @@
+package cnpm.nlu.be_empolyy_assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeEmpolyyAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeEmpolyyAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cnpm/nlu/be_empolyy_assessment/BeEmpolyyAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package cnpm.nlu.be_empolyy_assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeEmpolyyAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
